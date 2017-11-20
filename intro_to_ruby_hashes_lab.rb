5da54fd428142a821e5c9a5e8ba3c2f7ff8bc4a2
Neena van de Water <nkuriakose@hotmail.com>
2017-11-20 18:11:20 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,12 @@ def new_hash
 end
 
 def actor
-	actor = { :name => "Dwayne The Rock Johnson"}
+	actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {:railroads => {}
+	}
 
 end
 
